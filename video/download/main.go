fa198e90100affc77898bd43e5571bbb1a2ea8f5
Daniel Coo <danieljohncoo@gmail.com>
2024-06-14 08:43:28 -0400

    add-gcs: wip

--- video/download/main.go
+++ video/download/main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	// "context"
+	"context"
 	"fmt"
+	"io"
 	"log"
-	// "io"
 	"net/http"
 	"os"
 	"os/exec"
-	"strings"
-	// "path/filepath"
-	// "cloud.google.com/go/storage"
+	"path/filepath"
+
+	"cloud.google.com/go/storage"
 )
 
 func main() {
@@ -32,82 +32,73 @@ func main() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	cmd := exec.Command("/usr/local/bin/yt-dlp", "--version")
-	out, err := cmd.Output()
-
+	// Parse the request parameters
+	err := r.ParseForm()
 	if err != nil {
-		fmt.Println("yt-dlp does not exist:", err)
-	} else {
-		fmt.Println("yt-dlp version:", strings.TrimSpace(string(out)))
+		http.Error(w, "Failed to parse request parameters", http.StatusBadRequest)
+		return
 	}
 
-	// Parse the request parameters
-	// err := r.ParseForm()
-	// if err != nil {
-	// 	http.Error(w, "Failed to parse request parameters", http.StatusBadRequest)
-	// 	return
-	// }
-
 	// Get the value of the "url" parameter
-	// videoURL := r.FormValue("url")
-	// if videoURL == "" {
-	// 	http.Error(w, "Missing 'url' parameter", http.StatusBadRequest)
-	// 	return
-	// }
+	videoURL := r.FormValue("url")
+	if videoURL == "" {
+		http.Error(w, "Missing 'url' parameter", http.StatusBadRequest)
+		return
+	}
 
 	// Set the path to the "yt-dlp" binary
-	// ytdlpPath := "/usr/local/bin/yt-dlp"
+	ytdlpPath := "/usr/local/bin/yt-dlp"
 
 	// Set the additional flags for "yt-dlp"
-	// format := "bv*[ext=mp4]+ba[ext=m4a]/b[ext=mp4]"
-	// outputTemplate := "%(extractor)s-%(id)s.%(ext)s"
-	// targetDir := "/tmp"
-	// videoFilePath := filepath.Join(targetDir, outputTemplate)
+	format := "bv*[ext=mp4]+ba[ext=m4a]/b[ext=mp4]"
+	outputTemplate := "%(extractor)s-%(id)s.%(ext)s"
+	targetDir := "/tmp"
+	videoFilePath := filepath.Join(targetDir, outputTemplate)
 
 	// Create the "yt-dlp" command with the specified flags
-	// cmd := exec.Command(ytdlpPath, "--format", format, "-o", videoFilePath, "--restrict-filenames", videoURL)
+	cmd := exec.Command(ytdlpPath, "--format", format, "-o", videoFilePath, "--restrict-filenames", videoURL)
 
 	// Execute the "yt-dlp" command to download the video
-	// output, err := cmd.CombinedOutput()
-	// if err != nil {
-	// 	http.Error(w, fmt.Sprintf("Error downloading video: %s", string(output)), http.StatusInternalServerError)
-	// 	return
-	// }
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error downloading video: %s", string(output)), http.StatusInternalServerError)
+		return
+	}
 
 	// Create a new Cloud Storage client
-	// ctx := context.Background()
-	// client, err := storage.NewClient(ctx)
-	// if err != nil {
-	// 	http.Error(w, "Failed to create Cloud Storage client", http.StatusInternalServerError)
-	// 	return
-	// }
-	// defer client.Close()
+	ctx := context.Background()
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		http.Error(w, "Failed to create Cloud Storage client", http.StatusInternalServerError)
+		return
+	}
+	defer client.Close()
 
 	// Set the name of the Cloud Storage bucket
-	// bucketName := "videos-quarantine-2486aa1dcdb442fda0c2f090761b4479"
+	bucketName := "videos-quarantine-2486aa1dcdb442fda0c2f090761b4479"
 
 	// Open the downloaded video file
-	// videoFile, err := os.Open(videoFilePath)
-	// if err != nil {
-	// 	http.Error(w, "Failed to open video file", http.StatusInternalServerError)
-	// 	return
-	// }
-	// defer videoFile.Close()
+	videoFile, err := os.Open(videoFilePath)
+	if err != nil {
+		http.Error(w, "Failed to open video file", http.StatusInternalServerError)
+		return
+	}
+	defer videoFile.Close()
 
 	// Create a new object handle in the bucket
-	// obj := client.Bucket(bucketName).Object(filepath.Base(videoFilePath))
+	obj := client.Bucket(bucketName).Object(filepath.Base(videoFilePath))
 
 	// Upload the video file to Cloud Storage
-	// writer := obj.NewWriter(ctx)
-	// if _, err := io.Copy(writer, videoFile); err != nil {
-	// 	http.Error(w, "Failed to upload video to Cloud Storage", http.StatusInternalServerError)
-	// 	return
-	// }
-	// if err := writer.Close(); err != nil {
-	// 	http.Error(w, "Failed to close Cloud Storage writer", http.StatusInternalServerError)
-	// 	return
-	// }
+	writer := obj.NewWriter(ctx)
+	if _, err := io.Copy(writer, videoFile); err != nil {
+		http.Error(w, "Failed to upload video to Cloud Storage", http.StatusInternalServerError)
+		return
+	}
+	if err := writer.Close(); err != nil {
+		http.Error(w, "Failed to close Cloud Storage writer", http.StatusInternalServerError)
+		return
+	}
 
 	// Send a response back to the client
-	// fmt.Fprintf(w, "Video downloaded and uploaded to Cloud Storage bucket: %s", bucketName)
+	fmt.Fprintf(w, "Video downloaded and uploaded to Cloud Storage bucket: %s", bucketName)
 }
